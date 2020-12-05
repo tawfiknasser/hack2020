f49879a0f57891f7bae9b378b887cfcb0741c0df
Tawfik <tawfik@graymatters.health>
2020-12-05 12:51:21 +0200

    front1

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -1,5 +1,142 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="graph-box-report">
+        homeeeeee
+            <div ref="learning_curve"></div>
+
   </div>
 </template>
+<script>
+import Plotly from "plotly.js-basic-dist";
+import { mapGetters } from "vuex";
+export default {
+  name: "Graph",
+  components: {},
+  data() {
+    return {
+      learningCurveData: [
+        {
+          x: [],
+          y: [],
+          // error_y: {
+          //   type: "data",
+          //   array: [],
+          //   visible: true
+          // },
+          type: "scatter",
+          marker: { size: 1 }
+        }
+      ]
+    };
+  },
+  mounted() {
+      
+    //  Plotly.plot(this.$refs.learning_curve ,
+    //   [
+    //     {
+    //       y: [0],
+    //       mode: "lines",
+    //       line: { color: "#80CAF6" }
+    //     }
+    //   ],
+    //   this.layout,
+    //   {
+    //     displaylogo: false,
+    //     responsive: true
+    //   });
+
+
+    //  setInterval(()=>{
+    //   //    console.log("ffff");
+
+    //   //  // request
+    //   //  // fill learningCurveData
+    //   //  this.learningCurveData[0].x = [1,2,3,4,5]
+    //   //  this.learningCurveData[0].y= [1,6,3,1,5]
+
+    //   //  // plotly react
+    //   //   Plotly.react(
+    //   //       this.$refs.learning_curve,
+    //   //       [...this.learningCurveData],
+    //   //       this.layout
+    //   //     );
+    //  },1000)
+  },
+  created() {},
+  methods: {},
+  watch: {
+    getData : {
+      handler(){
+
+        console.log("dddd");
+        
+        console.log(   this.Gdata);
+        
+      }
+    }
+    // getScheduledSession: {
+    //   handler() {
+    //     if (this.getScheduledSession?.Report?.stat?.learning) {
+    //       const learningObject = this.getScheduledSession.Report.stat.learning;
+    //     const CycleIndexes = Object.keys(
+    //         this.getScheduledSession?.Report?.stat?.learning
+    //       );
+    //       for (const index of CycleIndexes) {
+    //         // y = learning_diff
+    //         // x = index
+    //         // error = learning_pooled_std
+    //         this.learningCurveData[0].x[index - 1] = index;
+    //         this.learningCurveData[0].y[index - 1] =
+    //           learningObject[index].score;
+    //         // this.learningCurveData[0].error_y.array[index] =
+    //         //   learningObject[index].learning_pooled_std;
+    //       }
+    //       Plotly.react(
+    //         this.$refs.learning_curve,
+    //         this.learningCurveData,
+    //         this.layout
+    //       );
+    //     }
+    //   }
+    // }
+  },
+  computed: {
+    ...mapGetters(["_$"]),
+    layout() {
+      return {
+        xaxis: {
+          title: {
+            text: "Average",
+            font: {
+              family: "Courier New, monospace",
+              size: 18,
+              color: "#7f7f7f"
+            }
+          }
+        },
+        yaxis: {
+        range : [65,215],
+          title: {
+            text: "Time",
+            font: {
+              family: "Courier New, monospace",
+              size: 18,
+              color: "#7f7f7f"
+            }
+          }
+        }
+      };
+    }
+  },
+  props: ["Gdata"],
+  destroyed: function() {},
+  beforeUpdate() {}
+};
+</script>
+<style  scoped>
+.first-row {
+  padding-top: 20px;
+  padding-left: 26px;
+  display: flex;
+  justify-content: space-between;
+}
+</style>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,14 +1,43 @@
 <template>
-  <div class="app">
+  <div class="graph-box-report">
+    homeeeeee
+    <Graph :Gdata="Gdata" />
   </div>
 </template>
-
 <script>
-
+import Plotly from "plotly.js-basic-dist";
+import { mapGetters } from "vuex";
+import Graph from "./About";
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    
-  }
-}
+    Graph
+  },
+  data() {
+    return {
+      Gdata : []
+    };
+  },
+  mounted() {
+    setInterval(() => {
+      console.log("request");
+      this.Gdata = [2]
+    }, 1000);
+  },
+  created() {},
+  methods: {},
+  watch: {},
+  computed: {},
+  props: [],
+  destroyed: function() {},
+  beforeUpdate() {}
+};
 </script>
+<style  scoped>
+.first-row {
+  padding-top: 20px;
+  padding-left: 26px;
+  display: flex;
+  justify-content: space-between;
+}
+</style>
